bffabb17db6e9e53afc70ff9f764e47a19124523
Anuj330 <96370716+Anuj330@users.noreply.github.com>
2022-12-22 06:07:28 +0530

    commit

--- KalyanFinalCut/settings.py
+++ KalyanFinalCut/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1t9977-m#4z==_+4_6a%%0t6yyc&lpjl8vc^poki+57_8_^55t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1',' ']
 
 
 # Application definition
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
